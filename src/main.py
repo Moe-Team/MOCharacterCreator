6e6054c3f3b621452436bf9e803a4308fcd0fc8b
MurdockHM <mmurdockh@gmail.com>
2017-11-30 15:05:45 +0100

    Set up skeleton

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+
+
+class CharacterCreatorApp(App):
+    use_kivy_settings = False
+
+    def build(self):
+        return
+
+
+if __name__ == '__main__':
+    CharacterCreatorApp().run()
