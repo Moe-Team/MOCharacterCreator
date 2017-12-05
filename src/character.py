00d75805c1dd15c0d3f9b94fa193e581ee2254c2
MurdockHM <mmurdockh@gmail.com>
2017-12-05 19:01:46 +0100

    Implement character loading and add Icarus

--- src/character.py
+++ src/character.py
@@ -1,5 +1,17 @@
 import os
 from kivy.config import ConfigParser
+from kivy.atlas import Atlas
+from icarus import Icarus
+
+
+class CharacterError(Exception):
+    pass
+
+
+class CharacterLoadingError(CharacterError):
+
+    def __init__(self, file):
+        self.file = file
 
 
 class CharacterDirParser:
@@ -8,22 +20,55 @@ class CharacterDirParser:
         self.dir = directory
 
     def find_settings(self):
+        settings = self.find_file("settings.ini")
+        return settings
+
+    def find_sprite_atlas(self):
+        return self.find_file("sprites.atlas")
+
+    def find_icon_atlas(self):
+        return self.find_file("icons.atlas")
+
+    def find_file(self, target):
         for file in os.listdir(self.dir):
-            if file == "settings.ini":
+            if file == target:
                 return os.path.join(self.dir, file)
+        return None
 
 
 class Character:
 
     def __init__(self, directory):
         self.dir_parser = None
+        self.sprites = None
+        self.icons = None
         self.name = self.extract_name(directory)
         self.config_parser = ConfigParser(self.name)
         self.load(directory)
 
     def load(self, directory):
         self.dir_parser = CharacterDirParser(directory)
+        self.load_settings()
+        self.load_sprites()
+        self.load_icons()
+
+    def load_icons(self):
+        icon_atlas_path = self.dir_parser.find_icon_atlas()
+        if icon_atlas_path is None:
+            raise CharacterLoadingError("icons.atlas")
+        self.icons = Atlas(icon_atlas_path)
+
+    def load_sprites(self):
+        sprite_atlas_path = self.dir_parser.find_sprite_atlas()
+        if sprite_atlas_path is None:
+            raise CharacterLoadingError("sprites.atlas")
+        self.sprites = Icarus(sprite_atlas_path)
+
+    def load_settings(self):
         settings = self.dir_parser.find_settings()
+        if settings is None:
+            raise CharacterLoadingError("settings.ini")
+        self.config_parser.read(settings)
 
     def extract_name(self, directory):
         dirs = directory.split(os.sep)
--- src/character_loader.py
+++ src/character_loader.py
@@ -1,4 +1,4 @@
-from character import Character
+from character import Character, CharacterLoadingError
 from kivy.uix.relativelayout import RelativeLayout
 
 
@@ -11,5 +11,9 @@ class CharacterLoader(RelativeLayout):
         self.character_display = character_display
 
     def load_character(self, directories):
-        self.character = Character(directories[0])
+        try:
+            self.character = Character(directories[0])
+        except CharacterLoadingError as e:
+            print("File not found: {0}".format(e.file))
+            return
         self.character_display.character = self.character
