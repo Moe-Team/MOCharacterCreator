9fa149afd967b123519411e7c5ce7895da00c6cf
MurdockHM <mmurdockh@gmail.com>
2017-12-08 20:22:51 +0100

    Add settings

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import kivy_settings
 from kivy.app import App
 from kivy.lang.builder import Builder
 from root_layout import RootLayout
+import os
 
 
 Builder.load_file('character_loader.kv')
@@ -14,6 +15,14 @@ class CharacterCreatorApp(App):
 
     def build(self):
         return RootLayout()
+
+    def build_config(self, config):
+        config.setdefaults('path', {
+            'root_path': os.getcwd()
+        })
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Main', self.config, 'settings.json')
 
 
 if __name__ == '__main__':
