4fd5d226ec06a4088b8746b65f516236f61db06b
MurdockHM <mmurdockh@gmail.com>
2017-12-12 14:50:42 +0100

    Refactor character_loader
    Remove duplication via inheritance

--- src/character_loader.py
+++ src/character_loader.py
@@ -2,67 +2,55 @@ from character import AtlasedCharacter, CharacterLoadingError, PNGCharacter
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import ObjectProperty
 from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 
 class CharacterLoader(RelativeLayout):
-    pass
-
-
-class AtlasedCharacterLoader(CharacterLoader):
     filechooser = ObjectProperty(None)
 
     def __init__(self, cancel_func, character_display, root, **kwargs):
-        super(AtlasedCharacterLoader, self).__init__(**kwargs)
+        super(RelativeLayout, self).__init__(**kwargs)
         self.character = None
         self.cancel = cancel_func
         self.character_display = character_display
         self.root = root
         self.ready()
 
-    def ready(self):
-        config = App.get_running_app().config
-        self.filechooser.path = config.get('path', 'root_path')
-        self.filechooser.bind(path=self.update_popup_title)
+    def display_error_popup(self, e):
+        label = Label(text="Missing file: {}".format(e.file))
+        popup = Popup(title="Error loading character", content=label)
+        popup.open()
 
     def update_popup_title(self, inst, val):
         popup = self.root.popup
         popup.title = val
 
+    def ready(self):
+        config = App.get_running_app().config
+        self.filechooser.path = config.get('path', 'root_path')
+        self.filechooser.bind(path=self.update_popup_title)
+
+
+class AtlasedCharacterLoader(CharacterLoader):
+
     def load_character(self, directories):
         try:
             self.character = AtlasedCharacter(directories[0])
         except CharacterLoadingError as e:
-            print("File not found: {0}".format(e.file))
+            self.display_error_popup(e)
             return
         self.character_display.character = self.character
         self.cancel(self.filechooser.path)
 
 
 class PNGCharacterLoader(CharacterLoader):
-    filechooser = ObjectProperty(None)
-
-    def __init__(self, cancel_func, character_display, root, **kwargs):
-        super(PNGCharacterLoader, self).__init__(**kwargs)
-        self.character = None
-        self.cancel = cancel_func
-        self.character_display = character_display
-        self.root = root
-        self.ready()
-
-    def ready(self):
-        config = App.get_running_app().config
-        self.filechooser.path = config.get('path', 'root_path')
-        self.filechooser.bind(path=self.update_popup_title)
-
-    def update_popup_title(self, inst, val):
-        popup = self.root.popup
-        popup.title = val
 
     def load_character(self, directories):
         try:
             self.character = PNGCharacter(directories[0])
         except CharacterLoadingError as e:
-            print("File not found: {0}".format(e.file))
+            self.display_error_popup(e)
             return
         self.character_display.character = self.character
         self.cancel(self.filechooser.path)
